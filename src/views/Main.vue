954dd00ae149ca232ac339dae3f732c4b3155873
Linus Willner <hello@linuswillner.me>
2021-05-05 15:03:09 +0300

    Use store.commit for logouts

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -46,6 +46,7 @@ import { API_BASE_URL, determineRequestErrorReason, joinUrl, RequestFailureReaso
 import store from '../store'
 import logHTTPRequestError from '../utils/logHTTPRequestError'
 import IDaemon from '../types/Daemon'
+import router from '../router'
 
 @Options({
   components: {
@@ -101,13 +102,7 @@ export default class Main extends Vue {
       const { reason, code } = determineRequestErrorReason(err)
 
       if (reason === RequestFailureReason.RECEIVED_ERROR_RESPONSE && code === ResponseCode.UNAUTHORIZED) {
-        // FIXME: Temp
-        // router.push('/login')
-        await axios.post(joinUrl(API_BASE_URL, 'user/login'), {
-          username: 'root',
-          password: 'password'
-        })
-        console.log('HACK: Session token had expired, reload and it will work now')
+        store.commit('logout')
       } else {
         logHTTPRequestError(err)
       }
